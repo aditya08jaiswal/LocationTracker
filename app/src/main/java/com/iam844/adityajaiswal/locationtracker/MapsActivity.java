a11bb9880432b019d3212317aa4af1cb40a12f43
iam844 <jaiswaladitya844@gmail.com>
2018-09-02 11:27:58 +0530

    Add email password activity

--- app/src/main/java/com/iam844/adityajaiswal/locationtracker/MapsActivity.java
+++ app/src/main/java/com/iam844/adityajaiswal/locationtracker/MapsActivity.java
@@ -9,16 +9,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    //Firebase
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +23,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
-        mAuth = FirebaseAuth.getInstance();
     }
 
 
